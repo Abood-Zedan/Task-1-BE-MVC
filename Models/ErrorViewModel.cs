dab4f0fe0876cf039207f430afaf2e2e2ce74629
Abood Zedan <155024024+Abood-Zedan@users.noreply.github.com>
2025-06-11 14:27:30 +0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Task_1_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task_1_mvc.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(13)]
+        public string name { get; set; }
+        [Required]
+        [MinLength(9)]
+        public string description { get; set; }
+        [Required]
+        [Range(1000,90000)]
+        public decimal price { get; set; }
+        public string? img { get; set; }
+        public string? category { get; set; }
+    }
+}
